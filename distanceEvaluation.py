527cae050e8e24bbfcc20a3523fb0d9b16941d0d
Mavroudopoulos Giannis <mavroudo@csd.auth.gr>
2019-10-27 11:55:32 +0100

    save evaluation

---
+++ distanceEvaluation.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import matplotlib.pyplot as plt 
+import os
+
+def writeToFile(distances, filesName):
+    if os.path.exists(filesName):
+      os.remove(filesName)
+    else:
+      pass
+    with open(filesName, "a") as myfile:
+        for index,line in enumerate(distances):
+            print(index)
+            for distance in line:
+                myfile.write(str(distance)+",")
+            myfile.write("\n")
+
+def readFromFile(filesName):
+    distances=[]
+    with open(filesName, "r") as myfile:
+        for index,line in enumerate(myfile):
+            distances.append([])
+            for distance in line.split(","):
+                try:
+                    distances[-1].append(int(distance))
+                except:
+                    pass
+    return distances
+
+
+def plotDistanceDistribution(distances,plotTitle):
+    #find max and max of maxs
+    maxes=[max(d) for d in distances]
+    maxMax=max(maxes) 
+    #plot the distance distribution
+    countDistances=[0 for i in range(int(maxMax)+1)]
+    for trace in distances:
+        for distance in trace:
+            countDistances[int(distance)]+=1
+    countDistances[0]=0
+    #plotting
+    x=[i for i in range(1,len(countDistances)+1)]
+    plt.plot(x, countDistances) 
+    plt.xlabel('distance') 
+    plt.ylabel('instances') 
+    plt.title(plotTitle) 
+    plt.savefig(plotTitle+'.png')
