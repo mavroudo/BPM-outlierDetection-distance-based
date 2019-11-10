9fe5606ed1d827194b9da6febb6e4abf1d81a0a9
Mavroudopoulos Giannis <mavroudo@csd.auth.gr>
2019-11-10 10:30:57 +0100

    added a mtree for range queries

--- Distance.py
+++ Distance.py
@@ -6,6 +6,8 @@ def calculateDiffernce(vector1,vector2):
    for index in range(len(vector1)):
        rmse+=pow(vector1[index]-vector2[index],2)
    return round(math.sqrt(rmse/len(vector1)),4)
+
+
    
 
 def calculateDistances(dataVector):
@@ -17,3 +19,8 @@ def calculateDistances(dataVector):
             if j>i:
                 distances[i].append(calculateDiffernce(dataVector[i],dataVector[j]))                
     return distances
+
+def distanceMtree(v1,v2):
+    v1List=[float(i) for i in v1[1:-1].split(",")]
+    v2List=[float(i) for i in v2[1:-1].split(",")]
+    return calculateDiffernce(v1List,v2List)
